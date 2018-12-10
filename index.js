172fe5c0fc24246f8b4d196b2f5692ce2361c912
superq <qq1143094348@gmail.com>
2018-12-10 23:09:09 +0800

    write package.json, copu temp

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const path = require('path')
 const packageJson = require('./package.json')
 
 let projectName;
+const defaultBrowsers = [
+  '>0.2%',
+  'not dead',
+  'not ie <= 11',
+  'not op_mini all',
+];
 
 
 const program =  new commander.Command(packageJson.name)
@@ -45,17 +51,68 @@ const program =  new commander.Command(packageJson.name)
 
     /**isValidName */
     const originDir = process.cwd()
-    const root = path.resolve(projectName)
+    const appPath = path.resolve(projectName)
+    // 改变node process 目录  
+
     fse.ensureDirSync(projectName)
     fse.writeFileSync(
-      path.join(root, 'package.json'), 
+      path.join(appPath, 'package.json'), 
       JSON.stringify(packageJson, null, 2) + os.EOL
+    ) 
+
+    init(
+      appPath
     )
+
+
   }
 
-  function init() {
-    
+
+  //1 write package.json
+  //2 copy temp to destination
+
+  function init(appPath) {
+    const appPackagePath = path.join(appPath, 'package.json')
+    const appPackage = require(appPackagePath)
+
+    appPackage.dependencies = appPackage.dependencies || {}
+    appPackage.browserslist = defaultBrowsers
+    appPackage.eslintConfig = {
+      extends: 'react-app',
+    };
+    // reademe file ignore
+
+    fse.writeFileSync(
+      appPackagePath,
+      JSON.stringify(appPackage, null, 2) + os.EOL
+    )
+
+    // dest no temp 
+    const tempPath = path.join('.', 'template')
+    fse.copySync(tempPath, appPath)
+
+    // try to set .gitignore
+    // append if there's already a '.gitigore' file
+    try {
+      fse.moveSync(
+        path.join(appPath, 'gitignore'),
+        path.join(appPath, '.gitignore')
+      )
+    }catch(err) {
+      if(err.code === 'EEXIST') {
+        const data = fse.readFileSync(path.join(appPath, 'gitignore'))
+        fse.appendFileSync(path.join(appPath, '.gitignore'), data)
+        fse.unlinkSync(appPath, 'gitignore')
+      }else {
+        throw err
+      }
+    }
+
+
+
+
+
   }
 
-  console.log(projectName, process.argv)
+  //console.log(projectName, process.argv)
 
